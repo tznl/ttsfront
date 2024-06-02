3753bea8dec2928ed70e566e83c5627a07b9bd1e
tznl <napkin@tuta.io>
2024-06-02 09:27:49 -0400

    true initial

---
+++ main.py
@@ -0,0 +1,48 @@
+if __name__ == '__main__':
+    from RealtimeTTS import TextToAudioStream, CoquiEngine
+    import sounddevice, time, logging, sys, readline
+
+
+
+    #Setup
+
+    engine  = CoquiEngine(
+        model_name  = "tts_models/multilingual/multi-dataset/xtts_v2",
+        voices_path = "./voice",
+        language    = "en",
+        voice       = "nicole.wav",
+        level=logging.CRITICAL
+        )
+
+    stream  = TextToAudioStream(engine)
+    running = 1
+    
+    #Variables
+
+    fast_sentence_fragment = False 
+    
+    #Main loop
+
+    def dummy_generator(string):
+        yield string
+
+    def main():
+        while running:
+#            print('> ', end='')
+            text = input("> ")
+            stream.feed(dummy_generator(text))
+            stream.play(
+                log_synthesized_text=False,
+                tokenizer = "nltk",
+                context_size = 12,
+                force_first_fragment_after_words=20,
+                output_wavfile="output.wav")
+
+    print("You may start typing.")
+
+    main()
+        
+
+    #Cleanup
+
+    engine.shutdown()
