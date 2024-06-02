3259d99bb60d157c05336a9c78d1526ab0d98529
tznl <napkin@tuta.io>
2024-06-02 10:05:27 -0400

    step2

---
+++ step2.bat
@@ -0,0 +1,7 @@
+python.exe -m pip install --upgrade pip
+pip install RealtimeTTS
+pip install torch==2.3.0+cu118 torchaudio==2.3.0 --index-url https://download.pytorch.org/whl/cu118
+choco install ffmpeg
+pip install openai keyboard realtimestt
+pip install transformers==4.38.2
+pip install TTS
